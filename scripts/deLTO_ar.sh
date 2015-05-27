320d44652fb06bc8e0b872bbbd337b4f9b4b4382
Rami Gökhan Kıcı <gokhankici@gmail.com>
2015-05-26 17:12:16 -0700

    added bitcode .a -> x86 .a converter

---
+++ scripts/deLTO_ar.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+# scripts dir
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CONFIG="$CUR_DIR/config.py"
+
+if [[ $# -ne 1 ]]; then
+  >&2 echo "usage: $(basename $0) <static lib>"
+  exit -1
+fi
+
+# full path of the library
+LIB_DIR=$(cd "$(dirname "$1")" && pwd)
+LIB="$LIB_DIR/$(basename $1)"
+
+# tmp dir to create the archive
+TMP_DIR=`mktemp -d`
+
+# full path of "ar"
+AR="$CUR_DIR/ar"
+
+# llvm linker
+LLVM_BIN_DIR="$($CONFIG LLVM_BUILD_DIR)/Release+Asserts/bin"
+LLC="${LLVM_BIN_DIR}/llc"
+
+pushd $TMP_DIR > /dev/null
+
+# get the files inside the archive
+FILES=$($AR tP $LIB | awk '{if(NR>1)print}')
+
+# lower each bitcode into machine code
+for obj in $FILES; do
+  BN=$(basename $obj)
+  $LLC -filetype=obj $obj -o=${BN}.sd.o
+done
+
+# create the new library with the .a.sd extension
+NEW_LIB="${LIB}.sd"
+/usr/bin/ar rcs "$NEW_LIB" *.sd.o
+
+popd > /dev/null
+
+# remove the temporary directory
+rm -rf $TMP_DIR
+
